6b85ebd24dce6801228a6a4590c6aa45606f8338
Riley Hendrickson <rileyhe1@gmail.com>
2026-09-03 15:01:07 -0700

    adding seeding file for initial products and respective quantities

---
+++ inventory-service/src/main/resources/db/migration/V2__seed_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (name, price, quantity) VALUES
+('Product 1', 99.99, 3),
+('Product 2', 199.99, 10),
+('Product 3', 156.00, 10),
+('Product 4', 205.00, 10),
+('Product 5', 169.99, 10),
+('Product 6', 299.99, 10);
