9aa3802ae40c1a80b9df2ddee0f83bb13487ede5
Riley Hendrickson <rileyhe1@gmail.com>
2026-09-03 15:01:48 -0700

    creating product repository

--- inventory-service/src/main/java/flashsalesystem/inventoryservice/repositories/ProductRepository.java
+++ inventory-service/src/main/java/flashsalesystem/inventoryservice/repositories/ProductRepository.java
@@ -2,7 +2,17 @@ package flashsalesystem.inventoryservice.repositories;
 
 import flashsalesystem.inventoryservice.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long>
 {
+    @Modifying
+    @Query("UPDATE Product p SET p.quantity = p.quantity - :quantityRequested WHERE p.id = :id AND p.quantity >= :quantityRequested")
+    int reserveStock(@Param("id") Long id, @Param("quantityRequested") int quantityRequested);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.quantity = p.quantity + :quantityReturned WHERE p.id = :id")
+    int returnStock(@Param("id") Long id, @Param("quantityReturned") int quantityReturned);
 }
