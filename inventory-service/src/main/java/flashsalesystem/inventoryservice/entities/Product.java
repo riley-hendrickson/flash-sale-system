01c8eaf1d958a71cc9c170ab39d974f4dd72a247
Riley Hendrickson <rileyhe1@gmail.com>
2026-09-03 12:57:53 -0700

    adding entity class for products

--- inventory-service/src/main/java/flashsalesystem/inventoryservice/entities/Product.java
+++ inventory-service/src/main/java/flashsalesystem/inventoryservice/entities/Product.java
@@ -1,4 +1,25 @@
 package flashsalesystem.inventoryservice.entities;
 
-public class Product {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private int quantity;
 }
