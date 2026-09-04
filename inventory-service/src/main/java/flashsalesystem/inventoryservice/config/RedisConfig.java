a6b075c7447b6f0f8684371d855f35b93ec52c84
Riley Hendrickson <rileyhe1@gmail.com>
2026-09-04 14:10:19 -0700

    added redis config bean to customize serialization strategy and ttl duration for entries

--- inventory-service/src/main/java/flashsalesystem/inventoryservice/config/RedisConfig.java
+++ inventory-service/src/main/java/flashsalesystem/inventoryservice/config/RedisConfig.java
@@ -1,4 +1,26 @@
 package flashsalesystem.inventoryservice.config;
 
-public class RedisConfig {
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig
+{
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer()
+    {
+        return builder -> builder
+                .withCacheConfiguration("product",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(30))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJacksonJsonRedisSerializer.builder().build())))
+                .withCacheConfiguration("productList",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(60))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJacksonJsonRedisSerializer.builder().build())));
+    }
 }
