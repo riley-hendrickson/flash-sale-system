321da86dcca48f4908554ff4530725314769ee34
Riley Hendrickson <rileyhe1@gmail.com>
2026-09-04 14:09:28 -0700

    added EnableCaching annotation to main method for cacheable annotation and AOP support

--- inventory-service/src/main/java/flashsalesystem/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/flashsalesystem/inventoryservice/InventoryServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class InventoryServiceApplication
 {
     private static final Logger log = LoggerFactory.getLogger(InventoryServiceApplication.class);
